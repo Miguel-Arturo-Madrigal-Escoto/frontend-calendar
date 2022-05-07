5bc246836cc95bc1a025047cc97ec1cdda172ff4
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2022-05-07 14:05:44 -0500

    app.get(*)

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 
 app.use(express.static('public'));
+app.get('*', (req, res) => {
+    const path = require('path');
+    console.log('path',path)
+    console.log('dirname',__dirname)
+    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
 app.listen(server_port, server_ip_address, () => {
     console.log(`Applicacion corriendo en puerto ${ server_port }`);
 });
