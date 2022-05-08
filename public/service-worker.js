9795c7433d3c4712e58094cf1db5ac484e117a67
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2022-05-08 02:21:19 -0500

    correccion de las rutas de las apis

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ab6f0779798e685b8c08266ce311cd3.js"
+  "/precache-manifest.d0df58c38a4ed999e60a38dedd3a4eac.js"
 );
 
 workbox.clientsClaim();
